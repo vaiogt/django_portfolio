686c4d04995315ae452d080ed9dccbb032efc8ca
Angus Chen <2314728+vaiogt@users.noreply.github.com>
2023-10-12 17:16:51 +0800

    Add models and admin models

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
+from .models import *
+
+
 # Register your models here.
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TranType)
+class TranTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+class StockTransactionInline(admin.StackedInline):
+    model = StockTransaction
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    inlines = [StockTransactionInline, ]
+
+
+@admin.register(StockTransaction)
+class StockTransactionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Fund)
+class FundAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FundTransaction)
+class FundTransactionAdmin(admin.ModelAdmin):
+    pass
